92feee79e41b77a6f980d88aad6a591d051d38b6
Nachiket Prajapati <prajapatinachiket014@gmail.com>
2024-02-06 08:01:37 +0530

    Pointer basics

---
+++ Chapter-6/06_01_pointer_basic.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int i = 34;
+    int *j = &i;
+
+    printf("The value of i is %d\n", i);
+    printf("The value of i is %d\n", *j);
+    printf("The address of i is %u\n", &i);
+    printf("The address of i is %u\n", j);
+    printf("The address of j is %u\n", &j);
+    printf("The value of j is %d\n", *(&j));
+    return 0;
+}
